c2a707fb7b1e00138374453f833651f6f0633f7d
Ting Zuge <ting.zuge@gov.ab.ca>
2021-03-25 15:04:17 -0600

    Initial setup of an nx-tools repository with an ADSP node plugin.

---
+++ packages/nx-adsp-node/src/generators/application/generator.ts
@@ -0,0 +1,77 @@
+import {
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  getWorkspacePath,
+  names,
+  offsetFromRoot,
+  Tree,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import { ApplicationGeneratorSchema } from './schema';
+import init from '../init/generator';
+
+interface NormalizedSchema extends ApplicationGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  openshiftDirectory: string;
+  tenantRealm: string;
+  accessServiceUrl: string;
+}
+
+function normalizeOptions(
+  host: Tree,
+  options: ApplicationGeneratorSchema
+): NormalizedSchema {
+  const name = names(options.name).fileName;
+  const projectDirectory = name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = `${getWorkspaceLayout(host).appsDir}/${projectDirectory}`;
+  
+  const openshiftDirectory = 
+    `${path.dirname(getWorkspacePath(host))}/.openshift/${projectDirectory}`
+  
+  const tenantRealm = options.tenant;
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    openshiftDirectory,
+    tenantRealm,
+    accessServiceUrl: 'https://access.alpha.alberta.ca'
+  };
+}
+
+function addFiles(host: Tree, options: NormalizedSchema) {
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    template: '',
+  };
+  generateFiles(
+    host,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
+  generateFiles(
+    host,
+    path.join(__dirname, 'openshift'),
+    `${options.openshiftDirectory}`,
+    templateOptions
+  );
+}
+
+export default async function (host: Tree, options: ApplicationGeneratorSchema) {
+  
+  const normalizedOptions = normalizeOptions(host, options);
+  
+  await init(host, options);
+  
+  addFiles(host, normalizedOptions);
+  await formatFiles(host);
+}
