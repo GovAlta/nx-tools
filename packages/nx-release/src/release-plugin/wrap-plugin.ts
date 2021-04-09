7bf30d01f181156d7de2b9efabf8d36f5bb9f222
Ting Zuge <ting.zuge@gov.ab.ca>
2021-04-09 07:29:03 -0600

    fix(nx-release): template updated to match configuration expected by plugin and handle case in the plugin.

--- packages/nx-release/src/release-plugin/wrap-plugin.ts
+++ packages/nx-release/src/release-plugin/wrap-plugin.ts
@@ -11,25 +11,30 @@ export const wrapPlugin = (plugin: PluginFunction) => async (
   context: Context
 ) => {
   const {commits, logger} = context;
+  let filteredCommits = commits;
 
-  logger.log(`Filtering commits to those affecting ${project}...`);
-  
-  const paths = await getProjectChangePaths(project);
-  logger.log(`Resolved to paths: ${paths.join(', ')}...`);
+  if (!project) {
+    logger.log("Skipping filtering of commits; no 'project' in configuration...");
+  } else {
+    logger.log(`Filtering commits to those affecting ${project}...`);
+    
+    const paths = await getProjectChangePaths(project);
+    logger.log(`Resolved to paths: ${paths.join(', ')}...`);
 
-  const from = context.lastRelease?.gitHead;
-  const to = context.nextRelease?.gitHead;
+    const from = context.lastRelease?.gitHead;
+    const to = context.nextRelease?.gitHead;
 
-  const hashes = await getPathCommitHashes(project, paths, from, to);
-  const filteredCommits = commits.filter(
-    commit => {
-      const match = hashes.includes(commit.commit.long);
-      if (match) {
-        logger.log(`Including commit ${commit.commit.short}: ${commit.message}`);
+    const hashes = await getPathCommitHashes(project, paths, from, to);
+    filteredCommits = commits.filter(
+      commit => {
+        const match = hashes.includes(commit.commit.long);
+        if (match) {
+          logger.log(`Including commit ${commit.commit.short}: ${commit.message}`);
+        }
+        return match;
       }
-      return match;
-    }
-  );
-
+    );
+  }
+  
   return await plugin(pluginConfig, {...context, commits: filteredCommits});
 }
