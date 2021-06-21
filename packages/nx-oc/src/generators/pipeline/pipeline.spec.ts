d726a784e0a045aeb8c54d1291b30e67479112e5
tingzuge-dio <47162374+tingzuge-dio@users.noreply.github.com>
2021-06-21 11:33:43 -0600

    feat: github actions based pipeline in the oc pipeline generator. (#33)

--- packages/nx-oc/src/generators/pipeline/pipeline.spec.ts
+++ packages/nx-oc/src/generators/pipeline/pipeline.spec.ts
@@ -4,41 +4,83 @@ import generator from './pipeline';
 
 describe('Pipeline Generator', () => {
 
-  const options: Schema = { 
-    pipeline: 'test', 
-    infra: 'test-infra', 
-    envs: 'test-dev' 
-  }
-  
-  it ('can run', async (done) => {
-    const host = createTreeWithEmptyWorkspace();
-    await generator(host, options);
-    expect(host.exists('.openshift/Jenkinsfile')).toBeTruthy();
-    expect(host.exists('.openshift/environment.infra.yml')).toBeTruthy();
-    expect(host.exists('.openshift/environments.yml')).toBeTruthy();
+  describe('Jenkins', () => {
+    const options: Schema = { 
+      pipeline: 'test', 
+      type: 'jenkins',
+      infra: 'test-infra', 
+      envs: 'test-dev' 
+    }
     
-    done();
+    it ('can run', async (done) => {
+      const host = createTreeWithEmptyWorkspace();
+      await generator(host, options);
+      expect(host.exists('.openshift/Jenkinsfile')).toBeTruthy();
+      expect(host.exists('.openshift/environment.infra.yml')).toBeTruthy();
+      expect(host.exists('.openshift/environments.yml')).toBeTruthy();
+      
+      done();
+    });
+    
+    it ('can generate multiple envs', async (done) => {
+      const host = createTreeWithEmptyWorkspace();
+      await generator(host, { ...options, envs: 'test-dev test-test' });
+      expect(host.exists('.openshift/environments.yml')).toBeTruthy();
+      
+      const envs = host.read('.openshift/environments.yml').toString();
+      expect(envs).toContain('test-dev');
+      expect(envs).toContain('test-test');
+      
+      done();
+    });
+  
+    it ('can fail for duplicate env project', async (done) => {
+      const host = createTreeWithEmptyWorkspace();
+      await expect(
+        generator(host, { ...options, envs: 'test-dev test-dev' })
+      ).rejects.toThrow('Each environment must be a unique project.');
+  
+      done();
+    });
   });
   
-  it ('can generate multiple envs', async (done) => {
-    const host = createTreeWithEmptyWorkspace();
-    await generator(host, { ...options, envs: 'test-dev test-test' });
-    expect(host.exists('.openshift/environments.yml')).toBeTruthy();
+  describe('GitHub Actions', () => {
+    const options: Schema = { 
+      pipeline: 'test', 
+      type: 'actions',
+      infra: 'test-infra', 
+      envs: 'test-dev' 
+    }
     
-    const envs = host.read('.openshift/environments.yml').toString();
-    expect(envs).toContain('test-dev');
-    expect(envs).toContain('test-test');
+    it ('can run', async (done) => {
+      const host = createTreeWithEmptyWorkspace();
+      await generator(host, options);
+      expect(host.exists('.github/workflows/pipeline.yml')).toBeTruthy();
+      expect(host.exists('.openshift/environment.infra.yml')).toBeTruthy();
+      expect(host.exists('.openshift/environments.yml')).toBeTruthy();
+      
+      done();
+    });
     
-    done();
+    it ('can generate multiple envs', async (done) => {
+      const host = createTreeWithEmptyWorkspace();
+      await generator(host, { ...options, envs: 'test-dev test-test' });
+      expect(host.exists('.openshift/environments.yml')).toBeTruthy();
+      
+      const envs = host.read('.openshift/environments.yml').toString();
+      expect(envs).toContain('test-dev');
+      expect(envs).toContain('test-test');
+      
+      done();
+    });
+  
+    it ('can fail for duplicate env project', async (done) => {
+      const host = createTreeWithEmptyWorkspace();
+      await expect(
+        generator(host, { ...options, envs: 'test-dev test-dev' })
+      ).rejects.toThrow('Each environment must be a unique project.');
+  
+      done();
+    });
   });
-
-  it ('can fail for duplicate env project', async (done) => {
-    
-    const host = createTreeWithEmptyWorkspace();
-    await expect(
-      generator(host, { ...options, envs: 'test-dev test-dev' })
-    ).rejects.toThrow('Each environment must be a unique project.');
-
-    done();
-  })
 });
