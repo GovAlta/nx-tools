f34f45182d666510a4cbccd86a8744d5d791bbf9
tzuge <47162374+tzuge@users.noreply.github.com>
2022-07-22 18:50:32 -0600

    feat(semantic-release-nuget): adding a basic semantic release nuget plugin that doesn't build or modify csproj files.

---
+++ packages/semantic-release-nuget/src/plugin/publish.ts
@@ -0,0 +1,41 @@
+import { PluginFunction } from '@semantic-release/semantic-release';
+import * as execa from 'execa';
+import path = require('path');
+import { NugetPluginConfig } from './config';
+
+export const publish: PluginFunction = async (
+  config: NugetPluginConfig,
+  context
+) => {
+  const {
+    nupkgRoot,
+    source,
+    symbolSource,
+    skipDuplicate,
+    timeout,
+  } = config;
+  const { cwd, env, stdout, stderr } = context;
+
+  const basePath = nupkgRoot ? path.resolve(cwd, nupkgRoot) : cwd;
+
+  const args = [
+    'nuget',
+    'push',
+    '*.nupkg',
+    source ? `--source ${source}` : null,
+    env.NUGET_API_KEY ? `--api-key ${env.NUGET_API_KEY}` : null,
+    symbolSource ? `--symbol-source ${symbolSource}` : null,
+    env.NUGET_API_KEY ? `--symbol-api-key ${env.NUGET_API_KEY}` : null,
+    skipDuplicate ? '--skip-duplicate' : null,
+    timeout ? `--timeout ${timeout}` : null,
+  ].filter((v) => !!v);
+
+  const dotnetResult = execa('dotnet', args, { env, cwd: basePath });
+
+  dotnetResult.stdout.pipe(stdout, { end: false });
+  dotnetResult.stderr.pipe(stderr, { end: false });
+
+  await dotnetResult;
+
+  return false;
+};
