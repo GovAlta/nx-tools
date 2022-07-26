52154a85b72e70e1bab7ac1573950caf45b889ce
Ting Zuge <ting.zuge@gov.ab.ca>
2022-07-26 13:13:46 -0600

    feat(semantic-release-nuget): adding project configuration property for dotnet pack.

--- packages/semantic-release-nuget/src/plugin/config.ts
+++ packages/semantic-release-nuget/src/plugin/config.ts
@@ -3,6 +3,7 @@ import { PluginConfig } from '@semantic-release/semantic-release';
 export interface NugetPluginConfig extends PluginConfig {
   configuration: string;
   noBuild: boolean;
+  project: string;
   includeSymbols: boolean;
   includeSource: boolean;
   serviceable: boolean;
