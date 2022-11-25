888ef3d1bbeeb37c46543a301e924e2002e00ec9
Ting Zuge <ting.zuge@gov.ab.ca>
2022-11-25 16:43:39 -0700

    fix(nx-adsp): updating react app template for react 18 and updated ui components

--- packages/nx-adsp/src/generators/react-app/react-app.ts
+++ packages/nx-adsp/src/generators/react-app/react-app.ts
@@ -121,6 +121,7 @@ export default async function (host: Tree, options: Schema) {
     {},
     {
       '@abgov/react-components': '^4.1.0',
+      '@abgov/styles': '^1.0.0',
       '@types/react-router-dom': '~5.3.3',
       '@types/redux-mock-store': '~1.0.2',
       'html-webpack-plugin': '~5.5.0',
