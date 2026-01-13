ea66853f062fff69fe1523f5c0da52a405571964
tzuge <47162374+tzuge@users.noreply.github.com>
2026-01-13 11:06:32 -0700

    feat: update nx depdency to 22
    Also update pipeline to use trusted publisher for npm packages.
    
    BREAKING CHANGE: requires nx 22 dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
-const { getJestProjects } = require('@nx/jest');
+import { getJestProjectsAsync } from '@nx/jest';
 
-export default {
+export default async () => ({
   projects: [
-    ...getJestProjects(),
+    ...(await getJestProjectsAsync()),
     '<rootDir>/e2e/nx-adsp-e2e',
     '<rootDir>/e2e/nx-oc-e2e',
     '<rootDir>/e2e/nx-release-e2e',
   ],
-};
+});
